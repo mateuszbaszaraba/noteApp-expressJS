8f53a8cc29c09788437b9385224cd62ad9936131
Mateusz Baszaraba <mateuszbaszaraba@outlook.com>
2022-06-27 17:26:15 +0200

    fetched all notes

--- mongo.js
+++ mongo.js
@@ -21,21 +21,35 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema);
 
+// mongoose
+//   .connect(url)
+//   .then((result) => {
+//     console.log('connected');
+
+//     const note = new Note({
+//       content: 'HTML is Easy',
+//       date: new Date(),
+//       important: true,
+//     });
+
+//     return note.save();
+//   })
+//   .then(() => {
+//     console.log('note saved!');
+//     return mongoose.connection.close();
+//   })
+//   .catch((err) => console.log(err));
+
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log('connected');
-
-    const note = new Note({
-      content: 'HTML is Easy',
-      date: new Date(),
-      important: true,
+  .then((res) => {
+    console.log('1');
+    return Note.find({}).then((result) => {
+      console.log('2');
+      result.forEach((note) => console.log(note));
     });
-
-    return note.save();
   })
   .then(() => {
-    console.log('note saved!');
-    return mongoose.connection.close();
-  })
-  .catch((err) => console.log(err));
+    console.log('3');
+    mongoose.connection.close();
+  });
