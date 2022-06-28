f1aa867f4ded91cb6a448694ac90c5776212eccd
Mateusz Baszaraba <mateuszbaszaraba@outlook.com>
2022-06-28 21:22:27 +0200

    eliminated try-catch with express async errors dependence

--- controllers/notes.js
+++ controllers/notes.js
@@ -7,15 +7,11 @@ notesRouter.get("/", async (request, response) => {
 });
 
 notesRouter.get("/:id", async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id);
-    if (note) {
-      return response.json(note);
-    } else {
-      return response.status(404).end();
-    }
-  } catch (err) {
-    next(err);
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    return response.json(note);
+  } else {
+    return response.status(404).end();
   }
 });
 
@@ -34,36 +30,24 @@ notesRouter.post("/", async (request, response, next) => {
     date: new Date(),
   });
 
-  try {
-    const savedNote = await note.save();
-    response.status(201).json(savedNote);
-  } catch (err) {
-    next(err);
-  }
+  const savedNote = await note.save();
+  response.status(201).json(savedNote);
 });
 
 notesRouter.delete("/:id", async (request, response, next) => {
-  try {
-    await Note.findByIdAndRemove(request.params.id);
-    response.status(204).end();
-  } catch (err) {
-    next(err);
-  }
+  await Note.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
 notesRouter.put("/:id", async (request, response, next) => {
   const { content, important } = request.body;
 
-  try {
-    const updatedNote = await Note.findByIdAndUpdate(
-      request.params.id,
-      { content, important },
-      { new: true, runValidators: true, context: "query" },
-    );
-    response.json(updatedNote);
-  } catch (err) {
-    next(err);
-  }
+  const updatedNote = await Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" },
+  );
+  response.json(updatedNote);
 });
 
 module.exports = notesRouter;
